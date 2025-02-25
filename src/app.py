780f7276229c642dbdf9a9f7a29711d223b9c63a
Balázs Németh <6349914+abalage@users.noreply.github.com>
2025-02-25 21:53:04 +0100

    feat: use create_app() instead of bare statements

--- src/app.py
+++ src/app.py
@@ -1,18 +1,24 @@
 from flask import Flask
-from routes import bp as main_bp
 import os
+from routes import bp as main_bp
 
-app: Flask = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 
-# Set the secret key to a random value
-app.secret_key = os.urandom(24)
+def create_app() -> Flask:
+    app: Flask = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 
-# Ensure the upload folder exists
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    # Set the secret key to a random value
+    app.secret_key = os.urandom(24)
 
-app.register_blueprint(main_bp)
+    # Ensure the upload folder exists
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    app.register_blueprint(main_bp)
+
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
